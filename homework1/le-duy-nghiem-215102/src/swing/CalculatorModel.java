3af91237dafd5474ba866db622ed6efde77b8a9d
Nghiem Duy <nghiem.eo.bua.18@gmail.com>
2024-02-28 22:18:01 +0700

    homework1 le duy nghiem

---
+++ homework1/le-duy-nghiem-215102/src/swing/CalculatorModel.java
@@ -0,0 +1,23 @@
+package swing;
+
+
+
+public class CalculatorModel {
+    private int calValue = 0;
+    public void addTwoNumbers(int firstNumber, int secondNumber){
+        calValue = firstNumber + secondNumber;
+    }
+
+    public int getCalculationValue(){
+        return calValue;
+    }
+    public void subTwoNumbers(int firstNumber,int secondNumber){
+        calValue = firstNumber - secondNumber;
+    }
+    public void mulTwoNumbers(int firstNumber,int secondNumber){
+        calValue = firstNumber * secondNumber;
+    }
+    public void divTwoNumbers(int firstNumber,int secondNumber){
+        calValue = firstNumber / secondNumber;
+    }
+}
