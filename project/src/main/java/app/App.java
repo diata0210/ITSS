4b35eb96e67fcccf5e1dc6bbc67df81033b2f1d3
Datvu <vudatntnd@gmail.com>
2024-05-12 20:15:21 +0700

    demo/screentransition

--- project/src/main/java/app/App.java
+++ project/src/main/java/app/App.java
@@ -1,17 +1,21 @@
 package app;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public final class App extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass()
+                .getResource("Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main (String []args){
+        launch(args);
     }
 }
