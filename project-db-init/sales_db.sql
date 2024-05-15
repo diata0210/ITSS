5ac473bf44fb2c2c28f4fce8a4a1bd9292ea039c
Nghiem Duy <nghiem.eo.bua.18@gmail.com>
2024-05-15 16:48:18 +0700

    docker change

--- project-db-init/sales_db.sql
+++ project-db-init/sales_db.sql
@@ -1,37 +1,37 @@
 -- Tạo cơ sở dữ liệu
-CREATE DATABASE IF NOT EXISTS sales_db;
-USE sales_db;
+CREATE DATABASE IF NOT EXISTS sale_db;
+USE sale_db;
 
 -- Creating table for `Site`
 CREATE TABLE IF NOT EXISTS Sites (
     ID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30),
-    address VARCHAR(30)
+    sname VARCHAR(30),
+    saddress VARCHAR(30)
 );
 
 -- Creating table for `Product`
 CREATE TABLE IF NOT EXISTS Products (
     ID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30),
-    type VARCHAR(30) 
+    names VARCHAR(30),
+    types VARCHAR(30)
 );
 
 -- Tạo bảng Đơn hàng
 CREATE TABLE IF NOT EXISTS Orders (
-  orderID INT AUTO_INCREMENT PRIMARY KEY,
+  ID INT AUTO_INCREMENT PRIMARY KEY,
   finalPrice INT,
   descriptions VARCHAR(1000),
-  sendDate
-  arriveDate
+  sendDate DATE,
+  arriveDate DATE
 );
 
 CREATE TABLE IF NOT EXISTS OrderDetail (
   productID INT,
   orderID INT,
   quantity INT,
-  FOREIGN KEY (siteOrderID) REFERENCES Sites(ID),
-  FOREIGN KEY (productID) REFERENCES Products(ID),
-)
+  FOREIGN KEY (orderID) REFERENCES Orders(ID),
+  FOREIGN KEY (productID) REFERENCES Products(ID)
+);
 
 CREATE TABLE IF NOT EXISTS ProductSite (
   ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -39,24 +39,24 @@ CREATE TABLE IF NOT EXISTS ProductSite (
   productID INT,
   quantity INT,
   FOREIGN KEY (siteID) REFERENCES Sites(ID),
-  FOREIGN KEY (productID) REFERENCES Products(ID),
-)
+  FOREIGN KEY (productID) REFERENCES Products(ID)
+);
 
 CREATE TABLE IF NOT EXISTS SiteOrder (
   ID INT AUTO_INCREMENT PRIMARY KEY,
   orderID INT,
   siteID INT,
   FOREIGN KEY (siteID) REFERENCES Sites(ID),
-  FOREIGN KEY (orderID) REFERENCES Orders(ID),
-)
+  FOREIGN KEY (orderID) REFERENCES Orders(ID)
+);
 
 CREATE TABLE IF NOT EXISTS SiteOrderDetail (
   ID INT AUTO_INCREMENT PRIMARY KEY,
   
   productID INT,
   quantity INT,
-  FOREIGN KEY (productID) REFERENCES Products(ID),
-)
+  FOREIGN KEY (productID) REFERENCES Products(ID)
+);
 
 
 
@@ -64,17 +64,17 @@ CREATE TABLE IF NOT EXISTS Users (
   username VARCHAR(255),
   upassword VARCHAR(255),
   ID INT,
-  roles INT,
-)
+  roles INT
+);
 
 CREATE TABLE IF NOT EXISTS Vehicles(
-  ID INT,
-  vname VARCHAR(30),
-)
-CREATE TABLE IF not EXISTS VehicleToSite (
+  ID INT AUTO_INCREMENT PRIMARY KEY,
+  vname VARCHAR(30)
+);
+CREATE TABLE IF NOT EXISTS VehicleToSite (
   ID INT,
   vehicleID INT,
   siteID INT,
   FOREIGN KEY (vehicleID) REFERENCES Vehicles(ID),
-  FOREIGN KEY (siteID) REFERENCES Sites(ID),
-)
+  FOREIGN KEY (siteID) REFERENCES Sites(ID)
+);
